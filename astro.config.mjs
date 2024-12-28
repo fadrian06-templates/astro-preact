5e399cd47abe80cde93c89a30c318942a40237de
fadrian06 <franyeradriansanchez@gmail.com>
2024-12-27 23:50:38 -0400

    npx astro add preact

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import preact from '@astrojs/preact'
 
 const __dirname = new URL(".", import.meta.url).pathname;
 const [, currentFolderName] = __dirname.split('/').reverse();
 
 // https://astro.build/config
 export default defineConfig({
-  base: `/${currentFolderName}/`
+  base: `/${currentFolderName}/`,
+  integrations: [preact()]
 });
